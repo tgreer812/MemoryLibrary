78d68a02e9340e6cb91dfa5a64afbcd1428dc2c3
Tyler Greer <tgreer812@gmail.com>
2023-04-20 20:15:14 -0400

    Updating look and feel of front end
    For now the mockup is in the Test folder

--- Test/script.js
+++ Test/script.js
@@ -2,23 +2,98 @@ const sidebar = document.getElementById('sidebar');
 
 // Replace this array with data fetched from your server or API.
 const agents = [
-    { id: 1, name: '127.0.0.1' },
-    { id: 2, name: 'Santiago' },
-    { id: 3, name: 'John' },
-    { id: 4, name: 'Lukas' },
-    { id: 5, name: 'Brad' },
+    { id: 1, name: 'Agent 1' },
+    { id: 2, name: 'Agent 2' },
+    { id: 3, name: 'Agent 3' },
+    { id: 4, name: 'Agent 4' },
+    { id: 5, name: 'Agent 5' },
     { id: 6, name: 'Agent 6' },
 ];
 
 agents.forEach(agent => {
     const button = document.createElement('button');
     const icon = document.createElement('i');
-    icon.className = 'fa fa-desktop'; // Font Awesome chat bubble icon
-    button.appendChild(icon); // Add icon to button
-    const textNode = document.createTextNode(' ' + agent.name); // Add a space before the agent's name for proper spacing
+    icon.className = 'fa fa-desktop';
+    button.appendChild(icon);
+    const textNode = document.createTextNode(' ' + agent.name);
     button.appendChild(textNode);
     button.addEventListener('click', () => {
         // Handle button click here (e.g., open agent chat or profile).
     });
     sidebar.appendChild(button);
+});
+
+// Add sample data to Table 1
+const table1 = document.getElementById('table1');
+const table1Data = [
+  ['0x00123456', '123', '100'],
+  ['0x00123457', '200', '150'],
+  ['0x00123458', '50', '25'],
+  ['0x00123456', '123', '100'],
+  ['0x00123457', '200', '150'],
+  ['0x00123458', '50', '25'],
+  ['0x00123456', '123', '100'],
+  ['0x00123457', '200', '150'],
+  ['0x00123458', '50', '25'],
+  ['0x00123456', '123', '100'],
+  ['0x00123457', '200', '150'],
+  ['0x00123458', '50', '25'],
+  ['0x00123456', '123', '100'],
+  ['0x00123457', '200', '150'],
+  ['0x00123458', '50', '25'],
+  ['0x00123456', '123', '100'],
+  ['0x00123457', '200', '150'],
+  ['0x00123458', '50', '25'],
+  ['0x00123456', '123', '100'],
+  ['0x00123457', '200', '150'],
+  ['0x00123458', '50', '25'],
+  ['0x00123456', '123', '100'],
+  ['0x00123457', '200', '150'],
+  ['0x00123458', '50', '25'],
+  ['0x00123456', '123', '100'],
+  ['0x00123457', '200', '150'],
+  ['0x00123458', '50', '25'],
+  ['0x00123456', '123', '100'],
+  ['0x00123457', '200', '150'],
+  ['0x00123458', '50', '25'],
+  ['0x00123456', '123', '100'],
+  ['0x00123457', '200', '150'],
+  ['0x00123458', '50', '25'],
+  ['0x00123456', '123', '100'],
+  ['0x00123457', '200', '150'],
+  ['0x00123458', '50', '25'],
+  ['0x00123456', '123', '100'],
+  ['0x00123457', '200', '150'],
+  ['0x00123458', '50', '25'],
+  ['0x00123456', '123', '100'],
+  ['0x00123457', '200', '150'],
+  ['0x00123458', '50', '25'],
+];
+
+table1Data.forEach(rowData => {
+  const row = document.createElement('tr');
+  rowData.forEach(cellData => {
+    const cell = document.createElement('td');
+    cell.textContent = cellData;
+    row.appendChild(cell);
+  });
+  table1.appendChild(row);
+});
+
+// Add sample data to Table 2
+const table2 = document.getElementById('table2');
+const table2Data = [
+  ['Yes', 'No Description', '0x00123456', 'Integer', '100'],
+  ['No', 'No Description', '0x00123457', 'Integer', '200'],
+  ['Yes', 'No Description', '0x00123458', 'Integer', '50'],
+];
+
+table2Data.forEach(rowData => {
+  const row = document.createElement('tr');
+  rowData.forEach(cellData => {
+    const cell = document.createElement('td');
+    cell.textContent = cellData;
+    row.appendChild(cell);
+  });
+  table2.appendChild(row);
 });
