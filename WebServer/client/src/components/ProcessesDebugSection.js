ab0b5e37658a005963e181b2f08eec896acf002b
Tyler Greer <tgreer812@gmail.com>
2023-05-01 17:59:24 -0400

    Update ProcessesDebugSection.js
    fixed it so that the processes update correctly when tasking the agent. I believe there is a bug where the agent doesn't correctly return to the tasking loop so that needs to be looked into

--- WebServer/client/src/components/ProcessesDebugSection.js
+++ WebServer/client/src/components/ProcessesDebugSection.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect, useCallback } from 'react';
-//import Table from './Table';
 import Backend from '../utility/backend';
 import ProcessTable from './ProcessTable';
 import './ProcessesDebugSection.css';
 
-const ProcessesDebugSection = ({ agentUUID }) => {
+const ProcessesDebugSection = ({ agentUUID, setPid }) => {
     const [processes, setProcesses] = useState([]);
+    const [processesSet, setProcessesSet] = useState(false);
 
     const fetchProcesses = async () => {
         if (agentUUID) {
@@ -18,13 +18,13 @@ const ProcessesDebugSection = ({ agentUUID }) => {
             const processes = await Backend.taskAgentByUUID(agentUUID, command, command_args);
             if (processes) {
                 return processes;
-            }  
+            }
         }
         return [];
     };
 
     const getProcesses = useCallback(async () => {
-        if (agentUUID) {
+        if (agentUUID && !processesSet) {
             const agent = await Backend.getAgentByUUID(agentUUID);
             if (agent) {
                 try {
@@ -36,10 +36,30 @@ const ProcessesDebugSection = ({ agentUUID }) => {
             }
         }
         return [];
-    }, [agentUUID]);
+    }, [agentUUID, processesSet]);
+
+    useEffect(() => {
+        if (agentUUID && !processesSet) {
+            setProcesses([]);
+            const pollProcesses = async () => {
+                const fetchedProcesses = await getProcesses();
+                if (fetchedProcesses.length > 0) {
+                    setProcesses(fetchedProcesses);
+                    setProcessesSet(true);
+                }
+            };
+            pollProcesses();
+            const intervalRef = setInterval(pollProcesses, 1000);
+
+            return () => {
+                clearInterval(intervalRef);
+            };
+        }
+    }, [agentUUID, getProcesses, processesSet]);
 
     const handleListProcessesButtonClick = async () => {
         alert("List Processes button clicked!");
+        setProcessesSet(false);
         let processes = await fetchProcesses();
         console.log("Processes: " + JSON.stringify(processes));
         setProcesses(processes);
@@ -47,15 +67,10 @@ const ProcessesDebugSection = ({ agentUUID }) => {
 
     const handleRowClick = (row, rowIndex) => {
         const pid = row[0]; // Assuming the PID is in the first column
-        alert(`Selected PID: ${pid}`);
-        // You can pass the PID up to the parent App component here
-      };
-
-    useEffect(() => {
-        (async () => {
-            setProcesses(await getProcesses());
-        })();
-    }, [agentUUID, getProcesses]);
+        //alert(`Selected PID: ${pid}`);
+        console.log(`Selected PID: ${pid}`);
+        setPid(pid);
+    };
 
     const headers = ['PID', 'Name'];
     const rows = processes.map((process) => [process.pid, process.name]);
